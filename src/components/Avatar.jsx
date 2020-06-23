553fc6971a993c049ea335a310f58bf995142ec5
atifbajwa <atifg7828@gmail.com>
2020-06-23 15:38:12 +0500

    adding avatar component in tweet component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+import avatar from './avatar.jpg'
+
+const Avatar = () => {
+    return (
+        <img src={avatar} alt="avatar" className="avatar"/>
+    )
+}
+
+export default Avatar
---
+++ src/components/Tweet.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css';
+
+// import components
+import Avatar from './Avatar'
+
+
+const Tweet = () => {
+    return (
+        <div className="tweet">
+            <Avatar />
+        </div >
+    )
+}
+
+export default Tweet
